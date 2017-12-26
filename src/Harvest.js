1078b6d3ecc26c36f0e80edc87175e53754b3b20
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2017-12-26 12:13:59 +0100

    Code optimisation, create a foreach loop to assign all new classes we create

--- src/Harvest.js
+++ src/Harvest.js
@@ -1,10 +1,7 @@
 let HeaderAuth = require('./authentication/HeaderAuth'),
     fs = require('fs'),
     path = require('path'),
-    camelCase = require('lodash/camelCase'),
-    Estimates = require('./api/Estimates'),
-    Estimate_Messages = require('./api/Estimate_Messages'),
-    Estimate_Item_Categories = require('./api/Estimate_Item_Categories');
+    camelCase = require('lodash/camelCase');
 
 module.exports = class Harvest {
     constructor(config) {
@@ -15,20 +12,12 @@ module.exports = class Harvest {
             headers: this.headerAuth.header()
         }
 
-        this.estimates = new Estimates(this.options);
-        this.estimateMessages = new Estimate_Messages(this.options);
-        this.estimateItemCategories = new Estimate_Item_Categories(this.options);
-    }
-
-
-    /*getAllApi() {
         fs.readdirSync(path.join(__dirname, 'api')).forEach(name => {
             let prop = camelCase(name.slice(0, -3));
             let Resource = require(`./api/${name}`);
 
-            //this[prop] = new(Resource)(this);
-            console.log(prop);
+            this[prop] = new(Resource)(this.options);
+            //console.log(this[prop]);
         });
-
-    }*/
+    }
 }
