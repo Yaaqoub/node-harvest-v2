fa5500033ef30b7153bf7b30d5486f693b7c6afd
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2017-12-28 09:51:06 +0100

    Added create and delete methods and it test unit to invoice payments

--- src/api/Invoice_Payments.js
+++ src/api/Invoice_Payments.js
@@ -13,4 +13,19 @@ module.exports = class Invoice_Payments {
 
         new Request(this.options, cb);
     }
+
+    create(id, params, cb) {
+        this.options.url = this.baseUri + id + '/' + this.name;
+        this.options.method = 'POST';
+        this.options.body = JSON.stringify(params);
+
+        new Request(this.options, cb);
+    }
+
+    delete(invoice_id, payment_id, cb) {
+        this.options.url = this.baseUri + invoice_id + '/' + this.name + '/' + payment_id;
+        this.options.method = 'DELETE';
+
+        new Request(this.options, cb);
+    }
 }
--- test/invoice_payments.js
+++ test/invoice_payments.js
@@ -4,7 +4,7 @@ let assert = require('assert'),
 let harvest = config_auth.harvest;
 
 describe('Invoice Payments API', function() {
-    describe('Get All Invoice Payments', function() {
+    describe('List All Invoice Payments', function() {
         it('should return all Invoice Payments', function(done) {
             harvest.invoicePayments.list(14729039, function(error, response, invoice_payments) {
                 //assert(!error);
@@ -12,6 +12,20 @@ describe('Invoice Payments API', function() {
                 //assert.equal(typeof invoice_payments[0], 'object');
                 done();
             });
+        });
+    });
+
+    describe('Create Invoice Payments', function() {
+        it('should create an Invoice Payment', function(done) {
+            assert.equal(typeof harvest.invoicePayments.create, 'function');
+            done();
+        });
+    });
+
+    describe('Delete Invoice Payments', function() {
+        it('should Delete an Invoice Payment', function(done) {
+            assert.equal(typeof harvest.invoicePayments.delete, 'function');
+            done();
         });
     });
 });
