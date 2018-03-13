fec92afc85f4eaf51814e82d1612a2eaf463cd86
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-03-13 17:33:05 +0100

    added Factory to help with testing

---
+++ test/Factory.js
@@ -0,0 +1,24 @@
+let auth = require('./config.auth');
+
+let test_id;
+
+function Factory() {
+
+}
+
+Factory.prototype.createClient = function(params, cb) {
+
+}
+
+Factory.prototype.createProject = function(params, cb) {
+
+}
+
+Factory.prototype.cleanHarvest = function(params, cb) {
+
+}
+
+module.exports = {
+    Factory,
+    test_id
+};
