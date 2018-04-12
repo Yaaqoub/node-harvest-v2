565166d6a54696f01d490e67053eb66ec878c46c
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-04-12 12:04:36 +0200

    Small changes in factory.js

--- test/config.auth.js
+++ test/config.auth.js
@@ -1,9 +1,11 @@
 let Harvest = require('../src/Harvest');
+require('dotenv').load();
+
 
 const config = {
-    account_ID: '',
-    access_token: '',
-    user_agent: 'Harvest api'
+    account_ID: process.env.ACCOUNT_ID || '',
+    access_token: process.env.ACCESS_TOKEN || '',
+    user_agent: process.env.USER_AGENT || ''
 };
 
 const harvest = new Harvest(config);
--- test/factory.js
+++ test/factory.js
@@ -1,33 +1,21 @@
+var exports = module.exports = {};
+
 let auth = require('./config.auth'),
     harvest = auth.harvest;
 
 const random_number = Math.floor(Math.random() * 10000) + 1;
 
-const client_test_name = 'harvest_test_client_name_' + random_number;
-const project_test_name = 'harvest_test_project_name_' + random_number;
-
-function getID(results) {
-    if (results) {
-        return JSON.parse(results).id;
-    }
-    return null;
-}
-
-function generateRandomNames(name) {
-    return 'harvest_test_' + name + '_name_' + random_number;
-}
-
-function createClient (done, callback) {
+exports.createClient = function(callback) {
     harvest.clients.create({
         'name': generateRandomNames('client'),
         'currency': 'EUR'
     }, function(error, response, res) {
         callback(getID(res));
-        done();
+        //done();
     });
-}
+};
 
-function createProject (clientID, callback) {
+exports.createProject = function(clientID, callback) {
     harvest.projects.create({
         'client_id': clientID,
         'name': generateRandomNames('project'),
@@ -39,16 +27,19 @@ function createProject (clientID, callback) {
     }, function(error, response, res) {
         callback(getID(res));
     });
-}
-
-function cleanHarvest () {
-    console.log("cleanHarvest");
-}
-
-module.exports = {
-    createClient,
-    createProject,
-    cleanHarvest,
-    getID,
-    generateRandomNames
 };
+
+exports.cleanHarvest = function(params) {
+    console.log(params);
+};
+
+function generateRandomNames(name) {
+    return 'harvest_test_' + name + '_name_' + random_number;
+}
+
+function getID(results) {
+    if (results) {
+        return JSON.parse(results).id;
+    }
+    return null;
+}
