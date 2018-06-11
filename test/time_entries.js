2a9781eda9ceefac92f298d6e3c8a1c32e1c4349
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-06-11 15:45:00 +0200

    fixed time entries tests

--- test/time_entries.js
+++ test/time_entries.js
@@ -1,15 +1,74 @@
 let assert = require('assert'),
-    config_auth = require('./config.auth');
+    config_auth = require('./config.auth'),
+    factory = require('./factory');
 
 let harvest = config_auth.harvest;
 
+const CLIENT_NAME = factory.generateRandomNames('CLIENT_');
+const PROJECT_NAME = factory.generateRandomNames('PROJECT_');
+const TASK_NAME = factory.generateRandomNames('TASK_');
+
+let CLIENT_ID = null,
+    PROJECT_ID = null,
+    TASK_ID = null,
+    PROJECT_TASK_ASSIGNMENTS_ID = null,
+    TIME_ENTRY_ID = null;
 
 describe('Time Entries API', function() {
+
+    before(async() => {
+        const theClient = await harvest.clients.create({
+            name: CLIENT_NAME
+        });
+        CLIENT_ID = factory.getID(theClient);
+
+        const theProject = await harvest.projects.create({
+            client_id: CLIENT_ID,
+            name: PROJECT_NAME,
+            is_billable: 'true',
+            bill_by: 'Project',
+            budget_by: 'project'
+        });
+        PROJECT_ID = factory.getID(theProject);
+
+        const theTask = await harvest.tasks.create({
+            name: TASK_NAME
+        });
+        TASK_ID = factory.getID(theTask);
+    });
+
+    after(async() => {
+        await harvest.tasks.delete(TASK_ID);
+        await harvest.projects.delete(PROJECT_ID);
+        await harvest.clients.delete(CLIENT_ID);
+    });
 
     describe('Create a time entry', function() {
         it('should implement Create a time entry method', (done) => {
             assert.equal(typeof harvest.timeEntries.create, 'function');
             done();
+        });
+
+        it('should Create a time entry', async() => {
+            assert(PROJECT_ID);
+            assert(TASK_ID);
+            /**
+             * Create Task Assignment
+             */
+            const theProjectTaskAssignments = await harvest.projectTaskAssignments.create(PROJECT_ID, {
+                task_id: TASK_ID
+            });
+            PROJECT_TASK_ASSIGNMENTS_ID = factory.getID(theProjectTaskAssignments);
+            assert.equal(typeof PROJECT_TASK_ASSIGNMENTS_ID, 'number', 'The response body should contain a id');
+
+            const timeEntry = await harvest.timeEntries.create({
+                'project_id': PROJECT_ID,
+                'task_id': TASK_ID,
+                'spent_date': '2018-06-11'
+            });
+
+            TIME_ENTRY_ID = factory.getID(timeEntry);
+            assert.equal(typeof TIME_ENTRY_ID, 'number', 'The response body should contain a id');
         });
     });
 
@@ -39,10 +98,16 @@ describe('Time Entries API', function() {
         });
     });
 
-    describe('Delete a time entry', function() {
-        it('should implement Delete a time entry method', (done) => {
-            assert.equal(typeof harvest.timeEntries.delete, 'function');
+    describe('Stop a running time entry', function() {
+        it('should implement Stop a running time entry method', (done) => {
+            assert.equal(typeof harvest.timeEntries.stop, 'function');
             done();
+        });
+
+        it('should Stop a running time entry', async() => {
+            assert(TIME_ENTRY_ID);
+            const timeEntry = await harvest.timeEntries.stop(TIME_ENTRY_ID);
+            assert(timeEntry);
         });
     });
 
@@ -51,12 +116,24 @@ describe('Time Entries API', function() {
             assert.equal(typeof harvest.timeEntries.restart, 'function');
             done();
         });
+
+        it('should Restart a stopped time entry', async() => {
+            assert(TIME_ENTRY_ID);
+            const timeEntry = await harvest.timeEntries.restart(TIME_ENTRY_ID);
+            assert(timeEntry);
+        });
     });
 
-    describe('Stop a running time entry', function() {
-        it('should implement Stop a running time entry method', (done) => {
-            assert.equal(typeof harvest.timeEntries.stop, 'function');
+    describe('Delete a time entry', function() {
+        it('should implement Delete a time entry method', (done) => {
+            assert.equal(typeof harvest.timeEntries.delete, 'function');
             done();
+        });
+
+        it('should Delete a time entry', async() => {
+            assert(TIME_ENTRY_ID);
+            await harvest.timeEntries.delete(TIME_ENTRY_ID);
+            await harvest.projectTaskAssignments.delete(PROJECT_ID, PROJECT_TASK_ASSIGNMENTS_ID);
         });
     });
 });
