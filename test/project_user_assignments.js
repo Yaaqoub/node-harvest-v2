3644a44e139ec7fc8dd82bf4af4458a96e6eb5d6
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-06-11 15:16:02 +0200

    fixed project task assignments tests

--- test/project_user_assignments.js
+++ test/project_user_assignments.js
@@ -1,9 +1,61 @@
 let assert = require('assert'),
-    config_auth = require('./config.auth');
+    config_auth = require('./config.auth'),
+    factory = require('./factory');
 
 let harvest = config_auth.harvest;
 
+const CLIENT_NAME = factory.generateRandomNames('CLIENT_');
+const PROJECT_NAME = factory.generateRandomNames('PROJECT_');
+
+let USER_ASSIGNMENT_ID = null,
+    USER_ID = null,
+    CLIENT_ID = null,
+    PROJECT_ID = null;
+
 describe('Project User Assignments API', function() {
+
+    before(async() => {
+        const theUser = await harvest.users.retrieve('me');
+        USER_ID = factory.getID(theUser);
+
+        const theClient = await harvest.clients.create({
+            name: CLIENT_NAME
+        });
+        CLIENT_ID = factory.getID(theClient);
+
+        const theProject = await harvest.projects.create({
+            client_id: CLIENT_ID,
+            name: PROJECT_NAME,
+            is_billable: 'true',
+            bill_by: 'Project',
+            budget_by: 'project'
+        });
+        PROJECT_ID = factory.getID(theProject);
+    });
+
+    after(async() => {
+        await harvest.projects.delete(PROJECT_ID);
+        await harvest.clients.delete(CLIENT_ID);
+    });
+
+    describe('Create a user assignment', function() {
+        it('should implement Create a user assignment method', (done) => {
+            assert.equal(typeof harvest.projectUserAssignments.create, 'function');
+            done();
+        });
+
+        it('should Create a user assignment', async() => {
+            assert(PROJECT_ID);
+            assert(USER_ID);
+            const userAssignment = await harvest.projectUserAssignments.create(PROJECT_ID, {
+                'user_id': USER_ID
+            });
+
+            USER_ASSIGNMENT_ID = factory.getID(userAssignment);
+            assert.equal(typeof USER_ASSIGNMENT_ID, 'number', 'The response body should contain a id');
+        });
+    });
+
     describe('List all user assignments', function() {
         it('should implement List all user assignments method', (done) => {
             assert.equal(typeof harvest.projectUserAssignments.list, 'function');
@@ -21,12 +73,13 @@ describe('Project User Assignments API', function() {
             assert.equal(typeof harvest.projectUserAssignments.retrieve, 'function');
             done();
         });
-    });
 
-    describe('Create a user assignment', function() {
-        it('should implement Create a user assignment method', (done) => {
-            assert.equal(typeof harvest.projectUserAssignments.create, 'function');
-            done();
+        it('should Retrieve a user assignment', async() => {
+            assert(PROJECT_ID);
+            assert(USER_ASSIGNMENT_ID);
+            const userAssignment = await harvest.projectUserAssignments.retrieve(PROJECT_ID, USER_ASSIGNMENT_ID);
+
+            assert(userAssignment);
         });
     });
 
@@ -35,12 +88,28 @@ describe('Project User Assignments API', function() {
             assert.equal(typeof harvest.projectUserAssignments.update, 'function');
             done();
         });
+
+        it('should Update a user assignment', async() => {
+            assert(PROJECT_ID);
+            assert(USER_ASSIGNMENT_ID);
+            const userAssignment = await harvest.projectUserAssignments.update(PROJECT_ID, USER_ASSIGNMENT_ID, {
+                budget: '120'
+            });
+
+            assert(userAssignment);
+        });
     });
 
     describe('Delete a user assignment', function() {
         it('should implement Delete a user assignment method', (done) => {
             assert.equal(typeof harvest.projectUserAssignments.delete, 'function');
             done();
+        });
+
+        it('should Update a user assignment', async() => {
+            assert(PROJECT_ID);
+            assert(USER_ASSIGNMENT_ID);
+            await harvest.projectUserAssignments.update(PROJECT_ID, USER_ASSIGNMENT_ID);
         });
     });
 });
