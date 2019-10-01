6f6791eef66b582c313f0867ad10ed7e571d1a0a
Yaaqoub <semlali.yaaqoub@gmail.com>
2019-10-02 00:00:38 +0200

    added user billable rates

---
+++ src/api/User_Billable_Rates.js
@@ -0,0 +1,13 @@
+let base = require('../mixins/Base2.js');
+
+let pick = require('lodash/pick.js');
+
+function User_Cost_Rates(options) {
+    this.name = 'billable_rates';
+    this.baseUri = 'https://api.harvestapp.com/v2/users/';
+    this.options = options;
+}
+
+Object.assign(User_Cost_Rates.prototype, pick(base, ['list', 'retrieve', 'create']));
+
+module.exports = User_Cost_Rates;
